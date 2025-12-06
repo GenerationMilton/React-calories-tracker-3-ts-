c8a827d7fe87cabbbc0081e153298ab581fccf59
Milton Munoz <livemilton@gmail.com>
2025-12-05 22:21:43 -0500

    calories-tracker :sparkles: actions in reducer

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,8 +1,7 @@
 import type { Activity } from "../types"
 
-export type ActivityActions = {
-
-}
+export type ActivityActions = 
+ {type: 'save-activity', payload: { newActivity: Activity } }
 
 type ActivityState = {
     activities: Activity[]
@@ -12,6 +11,14 @@ export const initialState: ActivityState = {
     activities: []
 }
 
-export const activityReducer = () =>{
+export const activityReducer = (
+    state : ActivityState = initialState,
+    action: ActivityActions
+    ) =>{
+
+        if(action.type === 'save-activity'){
+            // Este código maneja la logica para actualizar el state
+            console.log('desde el type de save-activity')
+        }
 
 }
