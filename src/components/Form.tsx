00806821189d139f93a48b020e8d27fd2222ef01
Milton Munoz <livemilton@gmail.com>
2025-12-05 22:40:51 -0500

    calories-tracker :sparkles: actions with dispatch

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,13 @@
+import { useState, ChangeEvent, type FormEvent, Dispatch } from "react";
 import { categories } from "../data/categories";
 import { Activity } from "../types";
-import { useState, ChangeEvent, type FormEvent } from "react";
+import type { ActivityActions } from "../reducers/activity-reducer";
 
-export default function Form() {
+type FormProps = {
+    dispatch: Dispatch<ActivityActions>
+}
+
+export default function Form({dispatch}: FormProps) {
 
     const[activity, setActivity] = useState<Activity>({
         category: 1,
@@ -27,7 +32,7 @@ export default function Form() {
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        console.log('Submit...')
+        dispatch({type: "save-activity", payload: {newActivity: activity}})
     }
 
   return (
