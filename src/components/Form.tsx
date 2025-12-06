0c1de1b5990f143e52d5ab91c543b67e85ab5b9f
Milton Munoz <livemilton@gmail.com>
2025-12-06 10:40:21 -0500

    calories-tracker :sparkles: Generate unique IDs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, type FormEvent, Dispatch } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import { categories } from "../data/categories";
 import { Activity } from "../types";
 import type { ActivityActions } from "../reducers/activity-reducer";
@@ -7,10 +8,11 @@ type FormProps = {
     dispatch: Dispatch<ActivityActions>
 }
 
-const initialState = {
-     category: 1,
-        name: '',
-        calories: 0
+const initialState: Activity = {
+    id: uuidv4(),
+    category: 1,
+    name: '',
+    calories: 0
 }
 
 export default function Form({dispatch}: FormProps) {
@@ -35,7 +37,10 @@ export default function Form({dispatch}: FormProps) {
         e.preventDefault()
 
         dispatch({type: "save-activity", payload: {newActivity: activity}})
-        setActivity(initialState)
+        setActivity({
+            ...initialState,
+            id: uuidv4()
+        })
     }
 
   return (
