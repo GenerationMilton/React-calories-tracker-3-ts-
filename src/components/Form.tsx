85dcfc308fe6e16d02aa3fc3479fc2afe3ece3dc
Milton Munoz <livemilton@gmail.com>
2025-12-05 22:03:11 -0500

    calories-tracker :sparkles: Defining our reduce

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,11 +11,10 @@ export default function Form() {
     })
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement> ) => {
-        const isNumberField = ['category', 'calories'].includes(e.target.id)
-
+        
         setActivity({
             ...activity,
-            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+            [e.target.id]: e.target.value
         })
        
     }
