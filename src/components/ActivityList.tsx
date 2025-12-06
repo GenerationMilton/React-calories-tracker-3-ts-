4ab87bfeed82846b8cd4415fa2212259df31bff3
Milton Munoz <livemilton@gmail.com>
2025-12-06 15:13:58 -0500

    calories-tracker :sparkles: Creating the button for remove action

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
 import { useMemo, Dispatch } from "react"
-import { PencilSquareIcon } from "@heroicons/react/24/outline" 
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline" 
 import type { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
@@ -43,6 +43,14 @@ export default function ActivityList({activities, dispatch}: ActivityListProps) 
                     >
                         <PencilSquareIcon
                             className="h-8 w-8 text-gray-800"
+                        />
+                    </button>
+
+                     <button
+                        onClick={() => dispatch({type: "delete-activity", payload: {id: activity.id}})}
+                    >
+                        <XCircleIcon
+                            className="h-8 w-8 text-red-500"
                         />
                     </button>
                 </div>
