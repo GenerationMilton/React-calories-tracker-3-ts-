6562d80547e30a50999f9b5bd904245f68ad5772
Milton Munoz <livemilton@gmail.com>
2025-12-06 15:46:59 -0500

    calories-tracker :sparkles: Creating the action to restart the app

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ function App() {
     localStorage.setItem('activities', JSON.stringify(state.activities))
   },[state.activities])
 
+  const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
+
   return (
     <>
       <header className="bg-lime-600 py-3">
@@ -18,6 +20,14 @@ function App() {
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+          <button
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer 
+            rounded-lg text-sm disabled:opacity-10"
+            disabled={!canRestartApp()}
+            onClick={()=> dispatch({type:'restart-app'})}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
       <section className="bg-lime-500 py-20 px-5">
